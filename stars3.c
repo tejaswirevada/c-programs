0159fd86cc141dfa1a611551f92990a3c66c41a0
tejaswirevada <tejaswirevada2006@gmail.com>
2025-12-14 14:39:52 +0530

    Add files via upload

---
+++ stars3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int n,i,j,k;
+	printf("\nEntetr n value:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=n;j>=i+1;j--)
+		{
+			printf(" ");
+		}
+		for(k=1;k<=i;k++)
+		{printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
