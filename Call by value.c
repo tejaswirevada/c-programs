272d24e4d26ba3dc8eff0bda0a6419a0de243fcc
tejaswirevada <tejaswirevada2006@gmail.com>
2025-12-15 10:42:11 +0530

    Add files via upload

---
+++ Call by value.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swap(int x,int y);
+int main()
+{
+	int a=10,b=20;
+	printf("\nBefore swapping a=%d,b=%d",a,b);
+	swap(&a,&b);
+	printf("\nAfter swapping a=%d,b=%d",a,b);
+	return 0;
+}
+void swap(int x,int y)
+{
+	int temp;
+	temp=x;
+	x=y;
+	y=temp;
+}
